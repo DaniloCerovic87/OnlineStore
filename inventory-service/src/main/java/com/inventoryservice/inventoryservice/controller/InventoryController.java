21bf57a9d71752db9d90688ffe752da9cb073051
Danilo <chessteacher87@gmail.com>
2025-09-02 15:49:54 +0200

    Return the response entity for inventory service

--- inventory-service/src/main/java/com/inventoryservice/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/inventoryservice/inventoryservice/controller/InventoryController.java
@@ -2,7 +2,7 @@ package com.inventoryservice.inventoryservice.controller;
 
 import com.inventoryservice.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +13,8 @@ public class InventoryController {
     private final InventoryService inventoryService;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
-        return inventoryService.isInStock(skuCode, quantity);
+    public ResponseEntity<Boolean> isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return ResponseEntity.ok().body(inventoryService.isInStock(skuCode, quantity));
     }
 
 }
