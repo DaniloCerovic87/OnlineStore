4f858391dea37304747ff1a8dd337a66d39b4cf1
Danilo <chessteacher87@gmail.com>
2025-09-17 14:13:28 +0200

    Improved error message in the case there is not enough quantity of that project in inventory

--- order-service/src/main/java/com/order/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/order/orderservice/service/OrderService.java
@@ -29,7 +29,10 @@ public class OrderService {
         var isProductInStock = inventoryClient.isInStock(request.skuCode(), request.quantity());
 
         if (!isProductInStock) {
-            throw new OutOfStockException("Product with SkuCode " + request.skuCode() + " is not in stock");
+            throw new OutOfStockException(
+                    "Product with SkuCode %s is not in stock (requested=%d)"
+                            .formatted(request.skuCode(), request.quantity())
+            );
         }
 
         Order order = new Order();
@@ -40,14 +43,14 @@ public class OrderService {
         orderRepository.save(order);
 
         // Send the message to Kafka Topic
-        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
-        orderPlacedEvent.setOrderNumber(order.getOrderNumber());
-        orderPlacedEvent.setEmail(request.userDetails().email());
-        orderPlacedEvent.setFirstName(request.userDetails().firstName());
-        orderPlacedEvent.setLastName(request.userDetails().lastName());
-        log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
-        kafkaTemplate.send("order-placed", orderPlacedEvent);
-        log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+//        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+//        orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+//        orderPlacedEvent.setEmail(request.userDetails().email());
+//        orderPlacedEvent.setFirstName(request.userDetails().firstName());
+//        orderPlacedEvent.setLastName(request.userDetails().lastName());
+//        log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+//        kafkaTemplate.send("order-placed", orderPlacedEvent);
+//        log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
 
         return new CreateOrderResponse(order.getId(), order.getOrderNumber(), order.getSkuCode(), order.getPrice(), order.getQuantity());
     }
