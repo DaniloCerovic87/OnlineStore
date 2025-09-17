b9f69719b8d5813fe474f59263e9a47d80508e5e
Danilo <chessteacher87@gmail.com>
2025-09-17 13:26:22 +0200

    Removed errorMessages property from ApiError object

--- order-service/src/main/java/com/order/orderservice/exception/response/ApiError.java
+++ order-service/src/main/java/com/order/orderservice/exception/response/ApiError.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
@@ -20,7 +19,5 @@ public class ApiError {
     private String message;
 
     private String debugMessage;
-
-    private List<String> errorMessages;
 
 }
