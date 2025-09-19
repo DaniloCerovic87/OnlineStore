d6e2e01c48b356f25cc78864dbee9b65600b71b4
Danilo <chessteacher87@gmail.com>
2025-09-19 21:56:07 +0200

    Inventory service - Moved MySQL to Docker volumes

--- inventory-service/src/main/java/com/inventoryservice/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/inventoryservice/inventoryservice/repository/InventoryRepository.java
@@ -4,5 +4,7 @@ import com.inventoryservice.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
     boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+
 }
