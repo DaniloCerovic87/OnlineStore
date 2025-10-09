08444f67ffd00830db31bc77f872a75bb34a521d
Danilo <chessteacher87@gmail.com>
2025-10-09 07:25:36 +0200

    Moving @EnableJpaAuditing from TestInventoryServiceApplication to InventoryServiceApplication

--- inventory-service/src/test/java/com/inventoryservice/inventoryservice/TestInventoryServiceApplication.java
+++ inventory-service/src/test/java/com/inventoryservice/inventoryservice/TestInventoryServiceApplication.java
@@ -1,9 +1,7 @@
 package com.inventoryservice.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 public class TestInventoryServiceApplication {
 
     public static void main(String[] args) {
